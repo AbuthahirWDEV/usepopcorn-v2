11e4d2fbf27b3f558923b2a0a2b0d395a32fc7ae
Abu <abu.dev.45@gmail.com>
2025-11-25 01:06:17 +0530

    completed star component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import Something from './StarRating';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import './index.css';
+import App from './App';
+import Something from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Test = () => {
+  const [moveieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <Something color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>The movie was rated {moveieRating} stars</p>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Something maxRating={5}/>
+    <App />
+
+    <Something maxRating={5} />
+    <Test />
   </React.StrictMode>
 );
-
